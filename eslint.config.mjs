60c35ab7e938800c2c047f2a9dc5eb5af21b7403
alihaggag11 <ali7aggag@gmail.com>
2025-02-26 17:12:47 +0200

    f2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,8 @@ const eslintConfig = [
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-case-declarations': 'off',
     },
   },
 ];
